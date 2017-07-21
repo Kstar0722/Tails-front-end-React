e151e24cf6f7ef3e9b1b34193ddf2334affa6635
Ivan Ivanov <Ivan@users-MacBook-Pro.local>
2017-07-22 00:07:10 +0300

    sign up page started (at new-page)

--- src/routes/NoAuth/NewPage/components/NewPage.js
+++ src/routes/NoAuth/NewPage/components/NewPage.js
@@ -7,7 +7,19 @@ class NewPage extends React.Component {
 
 	render() {
 		return (
-			<h1>New Page</h1>
+			<div id="signup" className="signup-wrap">
+				<div className="container">
+					<div className="title">
+						<h1>Sign Up</h1>
+						<p>I want to...</p>
+					</div>
+
+					<div className="signup-buttons-group">
+						<button className="button-ship">Ship</button>
+						<button className="button-carrier">Be a Carrier</button>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
