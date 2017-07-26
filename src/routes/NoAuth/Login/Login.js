ef6fc9d19643c7fcf1d91659412f58d91eacd4ba
thomas217 <thomas.wang217@gmail.com>
2017-07-26 22:00:12 +0200

    LoginForm is added

--- src/routes/NoAuth/Login/Login.js
+++ src/routes/NoAuth/Login/Login.js
@@ -1,13 +1,45 @@
-class Login extends React.Component {
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import LogInForm from './LogInForm';
+
+// import { loginUser } from '../actions/authActions';
+
+class Login extends Component {
+	
 	constructor(props) {
-		super(props)
+		super(props);
+		this.state = {
+
+		};
 	}
 
-	render() {
-		return (
-			<h1>Sign In</h1>
-		)
+	componentWillMount() {
+	    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+	handleSubmit(values) {
+		console.log(values)
+		// let { dispatch, history } = this.props
+		// const {email, password} = values
+		// dispatch(loginUser.request(email, password, history));
 	}
+
+  	render() {
+  		let pending = false
+		return (
+			<div className="login-container">
+				<LogInForm
+					onSubmit={this.handleSubmit}
+					isFetching={pending}
+				/>	
+			</div>
+		);
+  	}
 }
 
-export default Login
+const mapStateToProps = state => ({
+	// token: tokenSelector(state),
+	// pending: tokenPendingSelector(state)
+})
+
+export default connect(mapStateToProps)(Login);
