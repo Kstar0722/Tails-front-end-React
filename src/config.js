7158f8ad625d9fddaf4d1a0b619b7dde6c037455
Oleg <fotodon@mail.ru>
2017-10-13 12:40:52 +0300

    fix config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     endpoints: {
-        url: process.env.TAILS_API_URL || (process.env.NODE_ENV == 'production') ? 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com' : 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com',
+        url: (process.env.TAILS_API_URL) ? process.env.TAILS_API_URL : (process.env.NODE_ENV == 'production') ? 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com' : 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com',
         login: '/auth/signin',
         signup: '/auth/signup',
         forgot: '/api/auth/forgot_password',
