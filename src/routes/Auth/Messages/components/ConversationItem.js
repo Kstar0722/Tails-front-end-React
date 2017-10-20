de2ca67e9da5f6c284c26814223e01a6f6add701
AlexeyGlazunov <alekcei.glazunov@gmail.com>
2017-10-20 13:36:43 -0400

    load message data

---
+++ src/routes/Auth/Messages/components/ConversationItem.js
@@ -0,0 +1,32 @@
+import user from 'auth/user'
+
+class ConversationItem extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	stringifyNames(users) {
+		const id = parseInt(user.id), names = [];
+		let current;
+		for(let i = 0; i < users.length; i++) {
+			if(id != (current = users[i]).id) {
+				names.push(`${current.first_name} ${current.last_name}`);
+			}
+		}
+		return names.join(', ');
+	}
+
+	render() {
+  	const {id, name} = this.props
+		return (
+        <li onClick={this.props.selectEvent.bind(this.props.Messages, this.props.conversation)}>
+            <div className="avatar">
+                <img src="{FakeAvatar}" alt="" />
+            </div>
+            <h4 className="name"><span>{this.stringifyNames(this.props.conversation.users)}</span></h4>
+            {/*<p className="excerpt">Start a conversation</p>*/}
+        </li>
+        )
+    }
+}
+export default ConversationItem
--- src/routes/Auth/Messages/messages.js
+++ src/routes/Auth/Messages/messages.js
@@ -1,39 +1,52 @@
-import './messages.scss'
-import MessageFeed from './components/message-feed'
-import ChatList from './components/ChatList'
-import FakeAvatar from 'assets/avatar-50.png'
+import './Messages.scss'
+import { Link } from 'react-router';
+import ConversationItem from './components/ConversationItem'
+import apiService from '../../../lib/api'
+import user from 'auth/user'
 
 class Messages extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
+		this.state = {
+			conversations: [],
+			messages: [],
+			selected: null
+		};
 	}
 
-    getAllConversations() {
-		if (this.props.conversations) {
-			const conversations = []
-
-			this.props.conversations.data.map((conversation, i) => {
-				conversations.push(this.renderAllConversations(
-					conversation.id,
-					conversation.created_at
-				))
-			})
-
-			return conversations
-		}
+	selectConversation(conversation) {
+		this.setState({
+			selected: conversation,
+			messages: []
+		});
+		apiService.find('messages', {
+			filter: {
+				conversation_id: 1
+			}
+		}).then(res => {
+			this.setState({
+				messages: res.data
+			});
+		});
 	}
 
-    renderAllConversations(id,created_at) {
-		return (
-			<ChatList
-				id={id}
-				created_at={created_at}
-			/>
-		)
+	componentDidMount() {
+		apiService.find('conversations', {
+			include: ['users', 'listing']
+		}).then(res => {
+			this.setState({
+				conversations: res.data
+			});
+			if(res.data.length > 0) {
+				this.selectConversation(res.data[0]);
+			}
+		}).catch(function(error) {
+			console.error(error)
+		});
 	}
 
 	render() {
-        const { conversations } = this.props
+  	const { conversations } = this.props
 		return (
             <div className="container">
         		<div id="messages">
@@ -47,38 +60,61 @@ class Messages extends React.Component {
         				<div className="wrap">
         					<aside className="side">
         						<div className="title">
-        							<h4>Recent<span className="count">2</span></h4>
+        							<h4>Recent{/*<span className="count">2</span>*/}</h4>
         						</div>
         						<div className="scroller">
-        							<ChatList />
+											<ul className="messages-list">
+												{this.state.conversations.map(conversation =>
+													<ConversationItem key={conversation.id} conversation={conversation} Messages={this} selectEvent={this.selectConversation}/>
+												)}
+											</ul>
         						</div>
         					</aside>
 
-        					<div className="chat">
-        						<div className="chat-top">
-        							<div className="wrap">
-        								<div className="user">
-        									<h4 className="name">Alex Smith</h4>
-        								</div>
+        					{this.state.selected ? (
+										<div className="chat">
+											<div className="chat-top">
+												<div className="wrap">
+													<div className="user">
+														<h4 className="name">{this.state.selected.listing.title}</h4>
+													</div>
 
-        								<div className="buttons">
-        									<a href="#" className="btn-outline">Go to Listing</a>
-        									<a href="#" className="btn">View Profile</a>
-        								</div>
-        							</div>
-        						</div>
+													<div className="buttons">
+														<Link to={`/listing-details/${this.state.selected.listing.id}`} className="btn-outline">Go to Listing</Link>
+														{/*<a href="#" className="btn">View Profile</a>*/}
+													</div>
+												</div>
+											</div>
 
-        						<MessageFeed />
+											<div className="chat-window">
+					                <div className="scroller">
+					                    <ul className="chat-messages">
+																	{this.state.messages.map(message =>
+																			<li className={message.user_id == user.id ? 'me' : ''}><p>{message.message}</p></li>
+																	)}
+					                    </ul>
+					                </div>
+					            </div>
 
-        						<div className="write-message">
-        							<div className="wrap">
-        								<input type="text" className="field-message" placeholder="Type your message..." />
-        								<div className="buttons">
-        									<button className="btn-send"><i className="btm bt-paper-plane"></i></button>
-        								</div>
-        							</div>
-        						</div>
-        					</div>
+											<div className="write-message">
+												<div className="wrap">
+													<input type="text" className="field-message" placeholder="Type your message..." />
+													<div className="buttons">
+														<button className="btn-send"><i className="btm bt-paper-plane"></i></button>
+													</div>
+												</div>
+											</div>
+										</div>
+									) : (
+										<div className="chat no-message">
+											<div className="overlay">
+												<div className="text">
+													<h2>Hello</h2>
+													<p>To get started, choose a conversation from the left</p>
+												</div>
+											</div>
+										</div>
+									)}
         				</div>
         			</div>
         		</div>
