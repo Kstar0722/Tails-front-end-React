ac6cd1d34f9cb338f6b15dcc56212f8fabc0aab9
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-17 05:54:05 -0500

    update Avatar and FieldFormLine

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     endpoints: {
-        url: (process.env.TAILS_API_URL) ? process.env.TAILS_API_URL : (process.env.NODE_ENV == 'production') ? 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com' : 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com',
+        url: 'http://tails-api-dev.gpd2yfmmj5.us-west-2.elasticbeanstalk.com',
         login: '/auth/signin',
         signup: '/auth/signup',
         forgot: '/api/auth/forgot_password',
@@ -8,11 +8,6 @@ export default {
         userInfo: '/api/users/',
         changePass: '/api/users/',
         uerFood: '/api/user_diary_settings',
-        getCountry: '/api/locations',
-        listings: '/listings',
-        profile: '/users',
-        bids: '/bids',
-        listings_animals: '/listing_animals',
-        animal_image: '/animal_images'
+        getCountry: '/api/locations'
     }
 }
