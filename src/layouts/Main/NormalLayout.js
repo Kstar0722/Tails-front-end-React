ac1cd2e70e663939eb3d05454ded1046ff37a648
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-17 17:23:26 -0500

    change NormalLayouts

--- src/layouts/Main/NormalLayout.js
+++ src/layouts/Main/NormalLayout.js
@@ -1,20 +1,14 @@
-import { IndexLink, Link, Location } from 'react-router'
+import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import {connect} from 'react-redux'
 import './NormalLayout.scss'
 
 import Header from 'components/header/Header'
 import Footer from 'components/footer/Footer'
-import Notification from 'components/Notification'
 
-export const PageLayout = ({ children, location }) => (
+export const PageLayout = ({ children }) => (
   <div className='root'>
-    <Notification />
-    <div className={ classNames('main-wrap', {gray: location.pathname == '/sign-up'}) }>
-	    <Header />
-	    {children}  
-    </div>
+	<Header />
+	    {children}
     <Footer />
   </div>
 )
@@ -22,9 +16,4 @@ PageLayout.propTypes = {
   children: PropTypes.node,
 }
 
-export default connect(
-  state => ({
-	  location: state.location,
-  }),
-  {}
-)(PageLayout)
+export default PageLayout
