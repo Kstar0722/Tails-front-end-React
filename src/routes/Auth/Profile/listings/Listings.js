7d080863b8dd3464102f995797d114a9a76f2248
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-23 05:45:24 -0500

    update lingtings

--- src/routes/Auth/Profile/listings/Listings.js
+++ src/routes/Auth/Profile/listings/Listings.js
@@ -1,142 +1,53 @@
-import moment from 'moment'
-import { browserHistory } from 'react-router'
+import './Listings.scss'
+
 import ListingItem from './components/ListingItem'
-import Modal from 'react-modal'
-
-// import ReviewItem from "./ReviewItem"
-// import StepThree 
-
-let deletedID;
 
 class Listings extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			listingDatas: [],
-			deleteModal: false,
-		}
 	}
 
-	componentWillMount() {
-		const { listings } = this.props
-	}
-
-	getListings() {		
+	// Return the Listings
+	getListings() {
+		// for now, I'm just going to loop through and return 3
 		const listings = []
-		this.props.listings.data.map((listing, i) => {
-			listings.push(this.renderListings(
-				listing.id,
-				listing.title,
-				listing.budget,
-				moment(new Date(listing.created_at)).format('MM/DD/YYYY'),
-				{delete: this.delete.bind(this, listing.id)},
-				{review: this.review.bind(this, listing.id)},
-				{editStep: this.editStep.bind(this, listing.id)}
-			));
-		})
 
-		return listings
-	}
-	getIndex(value, arr, prop) {
-		for(var i = 0; i < arr.length; i++) {
-			if(arr[i][prop] === value) {
-				return i;
-			}
+		for (let i=0; i<3; i++) {
+			listings[i] = this.renderListings("Title", "Date", "Listing " + i, "Status")
 		}
-		return -1;
-	}
 
-	editStep(id){
-		var index = this.getIndex(id, this.props.listings.data, "id")
-		var val = this.props.listings.data[index]
-		console.log(val)
-		browserHistory.push({
-			pathname: '/step-three',
-			state: val
-		});
-	}
-
-	review(id)
-	{
-		console.log("view id =============================>")
-		console.log(id)
-		var index = this.getIndex(id, this.props.listings.data, "id")
-		var val = this.props.listings.data[index]
-		browserHistory.push({
-			pathname: '/profile/item-review',
-			state: val
-		});
-	}
-	delete(id){
-		this.setState({
-			deleteModal: true
-		})
-		deletedID = id
-	}
-	deleteItemFlagFun(flag)
-	{
-		this.setState({
-			deleteModal:false,
-		})
-		if(flag)
-		{
-			console.log("deletedID=========================================>")
-			console.log(deletedID)
-			this.props.deleteListing(deletedID);
-		}
+		return(
+			listings
+		)
 	}
 
 	// This will be called for each listing based on how many there are
-	renderListings(id, title, budget, date, optionActions, showActions, editStapActions) {
+	renderListings(title, date, bid, status) {
 		return (
 			<ListingItem
-				key={id}
-				id={id}
 				title={title}
-				budget={budget}
 				dateCreated={date}
-				optionActions={optionActions}
-				showActions = {showActions}
-				editStapActions = {editStapActions}
+				totalBids={bid}
+				status={status}
 			/>
 		)
 	}
 
+
 	render() {
 		return (
-			this.state.deleteModal?
-			<Modal isOpen={this.state.deleteModal}>
-				<div id="modal">
-					<div id ="heading-modal">
-						<button type="button" onClick={this.deleteItemFlagFun.bind(this, false)} className="close">X</button>
-						<p id="headtitle">Tails Transport</p>
-						
-					</div>	
-					<div id="body-modal">
-						<p id="body-text">Are you sure you want to delete this item?</p>
-					</div>
-					<div id="btn-modal">
-						<button id="btn-ok" onClick={this.deleteItemFlagFun.bind(this, true)}  >OK</button>
-						<button id="btn-cancel" onClick={this.deleteItemFlagFun.bind(this, false)} >Cancel</button>
-					</div>
-				</div>
-				
-			</Modal>:
-			
-			<table className="listings table table-bordered table-striped">
-				<thead>
+			<table className="listings">
+				<tbody>
 					<tr>
 						<th>Title</th>
-						<th>Budget</th>
-						<th>Date</th>
+						<th>Date Created</th>
+						<th>Total Bids</th>
+						<th>Status</th>
 						<th>Action</th>
-					</tr>					
-				</thead>
-				<tbody>
+					</tr>
 					{this.getListings()}
 				</tbody>
 			</table>
-			
 		)
 	}
 }
--- src/routes/Auth/Profile/listings/ListingsContainer.js
+++ src/routes/Auth/Profile/listings/ListingsContainer.js
@@ -1,13 +1,11 @@
 import Listings from './Listings'
-import { getListings, deleteListing } from 'actions/listing'
 
 const mapDispatchToProps = (dispatch) => ({
-    deleteListing: id => dispatch(deleteListing(id)),
+
 })
 
 const mapStateToProps = (state) => ({
-    listings: state.listing.data,
-    viewitem: state.params
+
 })
 
 export default ReactRedux.connect(mapStateToProps, mapDispatchToProps)(Listings)
