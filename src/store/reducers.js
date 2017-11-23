18937e8228a4ed7f0d4fd7f732c8144d4968b541
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-23 05:47:14 -0500

    upate

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,31 +1,11 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
-import { reducer as notifications } from 'react-notification-system-redux'
 import locationReducer from './location'
-import listingGetReducer from './listing'
-import authReducer from './auth'
-import profileReducer from './profile'
-import bidGetReducer from './bid'
-import animalsReducer from './animals'
-import animalShipReducer from './animalShip'
-import userReducer from './user'
-import listingBidReducer from './listing_bid'
-import listingAnimalsReducer from './listingAnimals'
+import { reducer as formReducer } from 'redux-form'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
-    form: formReducer,
-    notifications,
+  	form: formReducer,
     location: locationReducer,
-    auth: authReducer,
-    listing: listingGetReducer,
-    profile: profileReducer,
-    bid: bidGetReducer,
-    animalsReducer,
-    animalShipReducer,
-    user: userReducer,
-    listing_bid: listingBidReducer,
-    listingAnimals: listingAnimalsReducer,
     ...asyncReducers
   })
 }
