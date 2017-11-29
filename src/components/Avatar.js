0a478177edbee7e2404668b4f2c132e353526917
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-28 20:56:19 -0500

    update Avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,4 +1,6 @@
 import './Avatar.scss'
+import AvatarEditor from 'react-avatar-editor'
+import DefaultAvatar from 'assets/default_avatar.png'
 
 class Avatar extends React.Component {
 	constructor(props) {
@@ -17,8 +19,19 @@ class Avatar extends React.Component {
 		if (this.props.type == "large") {
 			return (
 				<div className="avatar-wrap d-flex flex-row flex-wrap justify-content-between align-items-center">
-					<img src="https://placeholdit.co//i/175x175?" />
-					<h2>{this.state.fName} {this.state.lName}</h2>
+					<img key={this.props.profile.avatar} className="styleImgAvatar" src={this.props.profile.avatar ? this.props.profile.avatar : DefaultAvatar} alt=''/>
+					{/*<AvatarEditor*/}
+							{/*ref={this.setEditorRef}*/}
+							{/*image={this.props.profile.avatar ? this.props.profile.avatar : "https://placeholdit.co//i/175x175?"}*/}
+							{/*width={175}*/}
+							{/*height={175}*/}
+							{/*position={{x:0.5,y:0.5}}*/}
+							{/*border={0}*/}
+							{/*color={[255, 255, 255, 0.6]} // RGBA*/}
+							{/*scale={this.props.profile.zoom_amount}	*/}
+						{/*/>*/}
+                    <h2>{this.props.profile.first_name} {this.props.profile.last_name}</h2>
+
 				</div>
 			)
 		}
@@ -28,4 +41,4 @@ class Avatar extends React.Component {
 	}
 }
 
-export default Avatar
+export default ReactRedux.connect((state) => { return {profile: state.profile.data}}, {})(Avatar)
