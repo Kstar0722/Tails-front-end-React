a7f18d8c1bbaf57ad733cbe49a2391568aa638b4
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-28 20:55:12 -0500

    action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,6 +4,9 @@ import {checkHttpStatus, parseJSON} from '../http.js'
 import config from '../config.js'
 import { browserHistory } from 'react-router'
 // import { dashboardUrl } from '../routes/urlGenerators'
+import user from 'auth/user'
+import Notifications from 'react-notification-system-redux'
+import { getProfile } from 'actions/profile'
 
 const LOGIN_REQUEST = 'LOGIN_REQUEST'
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
@@ -11,6 +14,9 @@ const LOGIN_FAILURE = 'LOGIN_FAILURE'
 const SIGNUP_REQUEST = 'SIGNUP_REQUEST'
 const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
 const SIGNUP_FAILURE = 'SIGNUP_FAILURE'
+const FORGOT_REQUEST = 'FORGOT_REQUEST'
+const FORGOT_SUCCESS = 'FORGOT_SUCCESS'
+const FORGOT_FAILURE = 'FORGOT_FAILURE'
 
 export function loginRequest() {
     return {
@@ -19,16 +25,20 @@ export function loginRequest() {
 }
 
 export function loginSuccess(res) {
+    user.authorize(res.access_token, res.user.id)
     return {
         type: LOGIN_SUCCESS,
-        userInfo: JSON.stringify(res)
+        userId: res.user.id
     }
 }
 
 export function loginFailure(error) {
     return {
-        type: LOGIN_FAILURE,
-        status: error.response
+        type: 'GET_NOTIFICATION',
+        notification: [{
+            type: 'danger',
+            message: 'Incorrect credentials'
+        }]
     }
 }
 
@@ -39,6 +49,7 @@ export function signupRequest() {
 }
 
 export function signupSuccess(res) {
+    user.authorize(res.access_token, res.user.id)
     return {
         type: SIGNUP_SUCCESS,
         userInfo: JSON.stringify(res)
@@ -47,10 +58,40 @@ export function signupSuccess(res) {
 
 export function signupFailure(error) {
     return {
-        type: SIGNUP_FAILURE,
-        status: error.response
+        type: 'GET_NOTIFICATION',
+        notification: [{
+            type: 'danger',
+            message: 'Incorrect credentials'
+        }]
     }
 }
+
+export function forgotRequest()
+{
+    return{
+        type: FORGOT_REQUEST
+    }
+}
+
+export function forgotSuccess(res) {
+    user.authorize(res.access_token, res.user.id)
+    return {
+        type: FORGOT_SUCCESS,
+        userInfo: JSON.stringify(res)
+    }
+}
+
+export function forgotFailure(error) {
+    return {
+        type: 'GET_NOTIFICATION',
+        notification: [{
+            type: 'danger',
+            message: 'Incorrect credentials'
+        }]
+    }
+}
+
+
 
 export function login(email, password) {
     return function(dispatch) {
@@ -65,15 +106,40 @@ export function login(email, password) {
         .then(checkHttpStatus)
         .then(parseJSON)
         .then(res => {
-            console.log("login success")
+            dispatch(loginSuccess(res))
+            browserHistory.push('/profile')
+            dispatch(getProfile())
         })
         .catch(error =>{
-            dispatch(loginFailure(error))
+           dispatch(Notifications.error({
+                title: '',
+                message: 'Incorrect credentials',
+                position: 'br',
+                autoDismiss: 3,
+           }));
         })
     }
 }
 
-export function register(firstName, lastName, email, password) {
+export function logout() {
+    return function(dispatch) {
+        dispatch({
+            type: 'LOGOUT'
+        })
+        dispatch({
+            type: 'PROFILE_UPDATE',
+            user: {}
+        })
+    }
+}
+
+export function register(username, email, password, purpose) {
+    let _username = '' + username.trim();
+    username = username.trim().split(' ');
+    let firstName = '' + username[0];
+    // let lastName = _username.slice(-1*firstName.length).trim();
+    let lastName = '' + username[1];
+
     return function(dispatch) {
         dispatch(signupRequest())
         return fetch(config.endpoints.url + config.endpoints.signup, {
@@ -85,16 +151,58 @@ export function register(firstName, lastName, email, password) {
                 first_name: firstName,
                 last_name: lastName,
                 email: email,
-                password: password
+                password: password,
+                purpose: purpose
             })
         })
         .then(checkHttpStatus)
         .then(parseJSON)
         .then(res => {
+            dispatch(signupSuccess(res))
+            dispatch(getProfile())
+            browserHistory.push('/profile')
             console.log("signup success")
         })
         .catch(error =>{
-            dispatch(signupFailure(error))
+            dispatch(Notifications.error({
+                title: '',
+                message: 'Incorrect credentials',
+                position: 'br',
+                autoDismiss: 3,
+           }));
         })
     }
+}
+
+export function forgotpass(email)
+{
+    return function(dispatch) {
+        dispatch(forgotRequest())
+        console.log(config.endpoints.url + config.endpoints.forgot)
+        return fetch(config.endpoints.url + config.endpoints.forgot, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email
+            })
+        })
+        .then(checkHttpStatus)
+        .then(parseJSON)
+        .then(res => {
+            dispatch(forgotSuccess(res))
+            // browserHistory.push('/profile')
+            console.log("forgot success")
+        })
+        .catch(error =>{
+            console.log(error)
+            dispatch(Notifications.error({
+                title: '',
+                message: 'Incorrect credentials',
+                position: 'br',
+                autoDismiss: 3,
+           }));
+        })
+    }    
 }
