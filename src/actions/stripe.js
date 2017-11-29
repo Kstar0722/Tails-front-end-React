18b2878909cdc9bd53ddf3abe69b3116ec4e7606
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-28 20:55:52 -0500

    update

--- src/actions/stripe.js
+++ src/actions/stripe.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import config from '../config.js'
-import { checkHttpStatus, parseJSON } from '../http.js'
-import user from 'auth/user'
-import Notifications from 'react-notification-system-redux'
-import apiService from '../lib/api'
-import { PROFILE_UPDATE, PROFILE_STRIPE} from '../config/actionTypes'
-
-export function createAccount() {
-    return function(dispatch) {
-      return apiService.post('users/create_stripe_account', {}).then(res => {
-       console.log('create_stripe_account',res)
-       dispatch({ type: PROFILE_UPDATE, user: res.user })
-      })
-      .catch(error => {
-        console.log('getBids err', error)
-      })
-    }
-}
-
-export function fetchStripeAccountInfo() {
-    return function(dispatch) {
-      return apiService.post('users/fetch_stripe_account_info').then(res => {
-       dispatch({ type: PROFILE_STRIPE, stripe: res.stripeData ? res.stripeData : {} })
-      })
-      .catch(error => {
-        console.log('getBids err', error)
-      })
-    }
-}
-
-export function verifyAccount(data) {
-    return function(dispatch) {
-      return apiService.post('stripe/verify_account', data).then(res => {
-      //  dispatch({ type: PROFILE_STRIPE, stripe: res.stripeData ? res.stripeData : {} })
-      console.log('verifyAccount', res)
-      })
-      .catch(error => {
-        console.log('getBids err', error)
-      })
-    }
-}
-
-=======
 import config from '../config.js'
 import { checkHttpStatus, parseJSON } from '../http.js'
 import user from 'auth/user'
@@ -87,4 +43,3 @@ export function verifyAccount(data) {
     }
 }
 
->>>>>>> 2f3f900230db1e3ff0f7af491adca4c8b7125de0
