a588659a410a1ff03c460e8ca51b7a52a50f8110
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-29 05:52:35 -0500

    update

--- src/components/fieldFormLine.js
+++ src/components/fieldFormLine.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-import classNames from 'classnames'
-
-class FieldForm extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentWillMount() {
-    
-  }
-
-  render() {
-    const { input, label, type, amount, options, meta: { touched, error, warning } } = this.props;
-
-
-    let input_component;
-
-    switch(type){
-      case 'textarea': 
-        input_component = <textarea {...input} className="form-control"></textarea>;
-        break;
-      case 'select':
-        input_component = <select className="form-control">
-          {options.map(item => <option value={item.value}>{item.label}</option>)}
-        </select>;
-        break;
-      default: 
-        input_component = <input {...input} type={type} className={classNames("form-control", {'form-control-danger': touched && error})}/>
-    }
-
-    return (
-      <div className={classNames("form-group row", {'has-danger': touched && error})}>
-        <label className="col-sm-4 col-form-label">{label}</label>
-        <div className="col-sm-6">
-          { input_component }
-          {touched && ((error && <small className="form-control-feedback">{error}</small>) || (warning && <span>{warning}</span>))}
-        </div>
-      </div>
-    )
-  }
-}
-
-=======
 import React, { Component } from 'react'
 import classNames from 'classnames'
 
@@ -87,5 +42,4 @@ class FieldForm extends Component {
   }
 }
 
->>>>>>> 2f3f900230db1e3ff0f7af491adca4c8b7125de0
 export default FieldForm;
