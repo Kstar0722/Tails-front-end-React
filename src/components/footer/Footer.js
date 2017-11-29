f962e20b322d0374b1df137f3de78b0365cb7005
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-29 05:52:54 -0500

    change footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import './Footer.scss'
-
+import user from 'auth/user'
 // import ImageLogo from 'components/logo/ImageLogo'
 
 import TextLogo from 'components/logo/TextLogo'
@@ -16,14 +16,17 @@ class Footer extends React.Component {
 			<section id="footer" className="d-flex align-items-center">
 				<div className="container">
 					<div className="footer-content d-flex flex-row flex-wrap justify-content-between align-items-center">
-					<TextLogo color="white"/>
+					<TextLogo/>
 						<nav className="hide-on-mobile">
 							<ul className="d-flex flex-row flex-wrap align-items-center justify-content-around">
+								<li className='menu-li-width'></li>
 								<li><Link to='/browse-jobs'>Browse Jobs</Link></li>
 								<li><Link to='/how-it-works'>How it Works</Link></li>
 								<li><Link to='/faq'>FAQ</Link></li>
 								<li><Link to='/support'>Support</Link></li>
-								<li className="sign-up"><Link className="hover-slide" to="/sign-up">Sign Up</Link></li>
+                {/*{!user.authorized ?*/}
+                  {/*<li className="sign-up"><Link className="hover-slide" to="/sign-up">Sign Up</Link></li>*/}
+									{/*: ''}*/}
 							</ul>
 						</nav>
 					</div>
@@ -33,7 +36,9 @@ class Footer extends React.Component {
 							<li><Link to='/how-it-works'>How it Works</Link></li>
 							<li><Link to='/faq'>FAQ</Link></li>
 							<li><Link to='/support'>Support</Link></li>
-							<li className="sign-up"><Link to="/sign-up">Sign Up</Link></li>
+              {/*{!user.authorized ?*/}
+							{/*<li className="sign-up"><Link to="/sign-up">Sign Up</Link></li>*/}
+								{/*:''}*/}
 						</ul>
 					</nav>
 				</div>
