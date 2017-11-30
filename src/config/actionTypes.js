8279312fc96058adfaa830046578fc83d8d813e5
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-11-30 05:58:51 -0500

    update notification

--- src/config/actionTypes.js
+++ src/config/actionTypes.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// PROFILE
-export const GET_PROFILE = 'GET_PROFILE';
-export const PROFILE_UPDATE = 'PROFILE_UPDATE';
-export const PROFILE_STRIPE = 'PROFILE_STRIPE';
-// ----------------------------------------------------
-
-// NOTIFICATION
-export const GET_NOTIFICATION = 'GET_NOTIFICATION';
-export const CLEAR_NOTIFICATION = 'CLEAR_NOTIFICATION';
-// -----------------------------------------------------
-
-// BIDS
-export const ADD_BID_SUCCESS = 'ADD_BID_SUCCESS';
-export const ADD_BID_ERROR = 'ADD_BID_ERROR';
-export const GET_BIDS_BY_LISTING_ID = 'GET_BIDS_BY_LISTING_ID';
-export const GET_LISTING_BIDS = 'GET_LISTING_BIDS';
-export const UPDATE_LISTING_BID = 'UPDATE_LISTING_BID';
-// ----------------------------------------------------
-
-// USER
-export const GET_USER = 'GET_USER';
-// -----------------------------------------------------
-
-// LISTINGS
-export const GET_COMPLETED_SHIPPING = 'GET_COMPLETED_SHIPPING';
-// -----------------------------------------------------
-
-// LISTING ANIMALS
-export const GET_LISTING_ANIMALS_BY_ID = 'GET_LISTING_ANIMALS_BY_ID';
-// -----------------------------------------------------
-
-export default {};
-=======
 // PROFILE
 export const GET_PROFILE = 'GET_PROFILE';
 export const PROFILE_UPDATE = 'PROFILE_UPDATE';
@@ -90,4 +56,3 @@ export const SET_TOTAL_PAGINATION = 'SET_TOTAL_PAGINATION';
 // -----------------------------------------------------
 
 export default {};
->>>>>>> 2f3f900230db1e3ff0f7af491adca4c8b7125de0
