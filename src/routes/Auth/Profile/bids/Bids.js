eaa5fecc1febd59e1cc5ab0fc4926fd4eb269c31
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-01 06:00:33 -0500

    change Bids

--- src/routes/Auth/Profile/bids/Bids.js
+++ src/routes/Auth/Profile/bids/Bids.js
@@ -1,5 +1,5 @@
 import './Bids.scss'
-
+import moment from 'moment'
 import BidItem from './components/BidItem'
 
 class Bids extends React.Component {
@@ -7,40 +7,46 @@ class Bids extends React.Component {
 		super(props)
 	}
 
-	// Return the bids
-	getBids() {
-		// for now, I'm just going to loop through and return 3
-		const bids = []
-
-		for (let i=0; i<3; i++) {
-			bids[i] = this.renderBids("Title", "Date", "Bid " + i, "Status")
-		}
-
-		return(
-			bids
-		)
+	componentWillMount() {
+      this.props.getBids()
 	}
 
-	// This will be called for each bid based on how many there are
-	renderBids(title, date, bid, status) {
+	getBids() {
+		const bids = []
+		this.props.bids.data.map((bid, i) => {
+			if(bid.listing != undefined){
+				bids.push(this.renderBids(
+					bid.id,
+					bid.listing.title,
+					moment(new Date(bid.created_at)).format('MM/DD/YYYY'),
+					bid.status,
+					bid.listing_id
+				))
+			}
+		})
+		return bids
+	}
+
+	renderBids(id, title, date, status, listing_id) {
 		return (
 			<BidItem
+				key={id}
+				id={id}
 				title={title}
 				dateBidded={date}
-				totalBids={bid}
 				status={status}
+				listing_id={listing_id}
 			/>
 		)
 	}
 
 	render() {
 		return (
-			<table className="bids">
+			<table className="bids table table-bordered table-striped">
 				<tbody>
 					<tr>
 						<th>Title</th>
 						<th>Date Bidded</th>
-						<th>Total Bids</th>
 						<th>Status</th>
 						<th>Action</th>
 					</tr>
