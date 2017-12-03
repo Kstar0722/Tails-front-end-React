2ded646a3bfc1feadb30a186f7d1526fa45f8d74
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-02 19:35:21 -0500

    change Biditem

--- src/routes/Auth/Profile/bids/components/BidItem.js
+++ src/routes/Auth/Profile/bids/components/BidItem.js
@@ -1,21 +1,30 @@
 import './BidItem.scss'
+import { Link } from 'react-router'
 
 class BidItem extends React.Component {
 	constructor(props) {
 		super(props)
 	}
 
+  renderBtnByStatus(){
+    switch (this.props.status){
+      case "accepted": return <td className="complete bold">Accepted</td>
+      case "rejected": return <td className="failed bold ">Rejected</td>
+      case null : return <td className="in-progress bold">Pending</td>
+        break
+      default: return <td className="bold ">None</td>
+    }
+  }
 	render() {
+		const { id, title, dateBidded, status, listing_id } = this.props
 		return (
-			<tr>
-				<td>{this.props.title}</td>
-				<td className="bold">{this.props.dateBidded}</td>
-				<td className="bold">{this.props.totalBids}</td>
-				<td className="in-progress bold">{this.props.status}</td>
+			<tr key={id}>
+				<td>{title}</td>
+				<td className="bold">{dateBidded}</td>
+        {this.renderBtnByStatus()}
 				<td className="actions">
-					<i className="fa fa-eye" aria-hidden="true"></i>
+					<Link to={"/listing-details/" + listing_id} className="link-color-eye"><i className="fa fa-eye" aria-hidden="true"></i></Link>
 					<i className="fa fa-pencil" aria-hidden="true"></i>
-					<i className="fa fa-trash-o" aria-hidden="true"></i>
 				</td>
 			</tr>
 		)
