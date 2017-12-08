b31d99939fb623ddf20a20173d073c4fc3d5066a
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-08 05:54:12 -0500

    update notification

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router'
+import { Link } from 'react-router'
 import './Notification.scss'
 import { connect } from 'react-redux'
 import {
@@ -14,7 +14,7 @@ import {
   DropdownItem,
   Popover,
   PopoverTitle,
-  PopoverContent
+  PopoverContent,
 } from 'reactstrap'
 import FaIconNotification from 'react-icons/lib/fa/bell-o'
 import { getNotification } from 'actions/notifications_bid'
@@ -32,10 +32,10 @@ class Notification extends Component {
   }
 
   toggle (type = 'dropdownOpen') {
-    let count = this.props.notifications.length;
-    if (count > 0 ){
+    let count = this.props.notifications.length
+    if (count > 0) {
       this.setState({
-        [type]: !this.state[type]
+        [type]: !this.state[type],
       })
     }
 
@@ -45,52 +45,58 @@ class Notification extends Component {
     this.props.getNotification()
   }
 
-  onclose(){
+  onclose () {
     this.setState({
-      popoverOpen: false
+      popoverOpen: false,
     })
   }
 
   render () {
-    let count = 0;
+    let count = 0
     this.props.notifications.map(notification => {
-      if(notification.bid){
-        count++;
+      if (notification.bid) {
+        count++
       }
     })
     return (
       <div>
-        
-          <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-						<DropdownToggle
-							tag="span"
-							onClick={this.toggle}
-							data-toggle="dropdown"
-							aria-expanded={this.state.dropdownOpen}
-						>
+
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+            onClick={this.toggle}
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
             <a id="Popover2" className="NotificationBell"
-             onClick={this.toggle}><FaIconNotification/></a>
-             <label className="labelCount" style={{display: count > 0 ? 'inline-block' : 'none' }}>{count}</label>
-						</DropdownToggle>
+               onClick={this.toggle}><FaIconNotification/></a>
+            <label className="labelCount" style={{
+              display: count > 0
+                ? 'inline-block'
+                : 'none',
+            }}>{count}</label>
+          </DropdownToggle>
           <DropdownMenu right style={{'top': '56px'}}>
-          <div className="afte-rec"></div>
-          {this.props.notifications.map((notification, index) => {
-                if(notification.notification_type == "offer_sent") {
+            <div className="afte-rec"></div>
+            {this.props.notifications.map((notification, index) => {
+                if (notification.notification_type == 'offer_sent') {
                   return <li key={index}>
                     <Link onClick={this.onclose}
-                          to={`/notification/${notification.id}`}>{notification.listing.user.first_name} has offered a
+                          to={`/notification/${notification.id}`}>{notification.listing.user.first_name} has
+                      offered a
                       shipment to you. Click here to accept</Link>
                   </li>
                 }
-                  if(notification.notification_type == "pending_peyment") {
-                    return <li key={index}>
-                      <Link onClick={this.onclose}
-                            to={`/notification/${notification.id}`}>{notification.bid.user.first_name} has accepted your offer! Click here to fund the shipment now'</Link>
-                    </li>
-                  }
-              }
-
-              )}
+                if (notification.notification_type == 'pending_peyment') {
+                  return <li key={index}>
+                    <Link onClick={this.onclose}
+                          to={`/notification/${notification.id}`}>{notification.bid.user.first_name} has
+                      accepted your offer! Click here to fund the shipment
+                      now'</Link>
+                  </li>
+                }
+              },
+            )}
           </DropdownMenu>
         </Dropdown>
 
@@ -105,6 +111,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const mapStateToProps = (state) => ({
   notifications: state.notifications_bid.data,
-  user : state.profile.data
+  user: state.profile.data,
 })
-export default ReactRedux.connect(mapStateToProps, mapDispatchToProps)(Notification)
+export default ReactRedux.connect(mapStateToProps, mapDispatchToProps)(
+  Notification)
