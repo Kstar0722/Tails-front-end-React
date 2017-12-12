c056007d7dcd7d4f0f72b88f8d784566450433bd
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-12 05:53:19 -0500

    update AddbitModal

--- src/routes/Auth/Listings/Modals/AddBidModal.js
+++ src/routes/Auth/Listings/Modals/AddBidModal.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from 'react'
 import './AddBidModal.scss'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import { Field, reduxForm } from 'redux-form'
-import { connect } from 'react-redux';
-import { addBid, getBidsByListingID } from "../../../../actions/bids";
+import { connect } from 'react-redux'
+import { addBid, getBidsByListingID } from '../../../../actions/bids'
+import {Link} from 'react-router'
 
 class AddBidModal extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       modal: false,
-      visibleButton: true
-    };
+      visibleButton: true,
+    }
 
-    this.toggle = this.toggle.bind(this);
+    this.toggle = this.toggle.bind(this)
   }
 
-  toggle() {
+  toggle () {
     this.setState({
-      modal: !this.state.modal
-    });
+      modal: !this.state.modal,
+    })
 
   }
 
@@ -30,31 +31,46 @@ class AddBidModal extends React.Component {
     bid.listing_id = this.props.id
 
     this.props.addBid(bid).then(() => {
-        this.toggle();
+      this.toggle()
     })
   }
 
-  render() {
-    let {user, bids, stripe} = this.props;
-    let visibleBt = true;
+  render () {
+    let { user, bids, stripe } = this.props
+    let visibleBt = true
+    console.log(stripe)
     bids.map(bid => {
-      if(user.id == bid.user_id){
-        visibleBt=false;
+      if (user.id == bid.user_id) {
+        visibleBt = false
       }
     })
 
-    let charges_enabled = false;
+    let charges_enabled = false
 
-    if(stripe.charges_enabled){
-      charges_enabled=true
+    if (typeof stripe.charges_enabled !== 'undefined') {
+      charges_enabled = true
     }
+
+    const completeNowMessage = (
+      <span>
+        You must complete your payout info before bidding
+        <Link to='/profile/edit'> click here to complete </Link>
+      </span>
+    )
 
     return (
       <div>
-        { visibleBt ? charges_enabled ? <Button color="danger" onClick={this.toggle}><span className="bid-now-btn">BID NOW</span></Button> : 'You must complete your payout info before bidding' : '' }
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <form onSubmit={this.handleSubmit} className="form-add-bid" >
-            <ModalHeader toggle={this.toggle}>{this.props.title} - $ {this.props.budget} Budget</ModalHeader>
+        {visibleBt
+          ? charges_enabled
+            ? <Button color="danger" onClick={this.toggle}><span
+              className="bid-now-btn">BID NOW</span></Button>
+            : completeNowMessage
+          : ''}
+        <Modal isOpen={this.state.modal} toggle={this.toggle}
+               className={this.props.className}>
+          <form onSubmit={this.handleSubmit} className="form-add-bid">
+            <ModalHeader toggle={this.toggle}>{this.props.title} -
+              $ {this.props.budget} Budget</ModalHeader>
             <ModalBody>
               <div>
                 <label>Bid Description</label>
@@ -62,7 +78,7 @@ class AddBidModal extends React.Component {
                   <Field
                     name="description"
                     component="textarea"
-                    required />
+                    required/>
                 </div>
               </div>
               <div>
@@ -72,7 +88,7 @@ class AddBidModal extends React.Component {
                     name="cost"
                     component="input"
                     type="number"
-                    required />
+                    required/>
                 </div>
               </div>
             </ModalBody>
@@ -82,19 +98,24 @@ class AddBidModal extends React.Component {
           </form>
         </Modal>
       </div>
-    );
+    )
   }
 }
 
 AddBidModal = reduxForm({
-  form: 'addBidForm'
+  form: 'addBidForm',
 })(AddBidModal)
 
 const mapStateToProps = state => ({
   addBidForm: state.form.addBidForm,
-  bids: state.bid.data ? Array.isArray(state.bid.data) ? state.bid.data : [] : [],
+  bids: state.bid.data
+    ? Array.isArray(state.bid.data)
+      ? state.bid.data
+      : []
+    : [],
   user: state.profile.data,
-  stripe: state.profile.stripe
+  stripe: state.profile.stripe,
 })
 
-export default connect(mapStateToProps, { addBid, getBidsByListingID } )(AddBidModal);
+export default connect(mapStateToProps, { addBid, getBidsByListingID })(
+  AddBidModal)
