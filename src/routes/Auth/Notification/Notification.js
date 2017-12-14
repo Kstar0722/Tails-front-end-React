917e7a10032801a8de77421587a9a65de0ec6aca
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-13 21:21:06 -0500

    chagne notification

--- src/routes/Auth/Notification/Notification.js
+++ src/routes/Auth/Notification/Notification.js
@@ -4,74 +4,44 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 import { approveBid } from 'actions/notifications_bid'
 import { Link } from 'react-router'
-import Payment from 'components/pay_bid/Payment';
+import Payment from 'components/pay_bid/Payment'
+import NotificationContent from './NotificationContent'
 
 class Notification extends Component {
   constructor (props) {
-
     super(props)
     this.state = {
-      isToggleOn: false
+      isToggleOn: false,
     }
     this.approve = this.approve.bind(this)
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this)
   }
 
   approve (id) {
     this.props.approveBid(id)
   }
 
-  handleClick() {
+  handleClick () {
     this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+      isToggleOn: !prevState.isToggleOn,
+    }))
   }
-
 
   render () {
     return (
-      <div className="container">
-        <div className="Notificaton">
+      <div className='container'>
+        <div className='notification'>
           {this.props.notifications.map((notification, index) => {
               if (notification.id == this.props.params.id) {
-                if(notification.notification_type == "offer_sent"){
-                  return<div key={index}>
-                    <h3 className="titleNotification"> {notification.listing.user.first_name} has ofer the
-                      job {notification.title}</h3>
-                    <p>This shipment must be picked up by  {moment(new Date(notification.bid.created_at)).format('MMMM Do YYYY')} and dropped off
-                      by {moment(new Date(notification.bid.updated_at)).format('MMMM Do YYYY')}</p>
-                    <p>The client has proposed this shipment for {notification.bid.budget} </p>
-                    <b>Other Notes:</b>
-                    <p>{notification.bid.description}</p>
-                    <p>Please ready the full terms here</p>
-                    <input onClick={this.handleClick} type="checkbox" />
-                    <a>I agree to the full terms</a>
-                    <div className="button_accept">
-                      <Link to='profile'> <button onClick={this.approve.bind(this, notification.bid.id)}
-                                                  disabled={ !(!notification.bid.details || !notification.bid.details.approved_by_bidder) || this.state.isToggleOn == false  }>I accept this shipment
-                      </button></Link>
-                    </div>
-                  </div>
-                }
+                return <NotificationContent
+                  notification={notification}
+                  key={index}
+                  handleClick={this.handleClick}
+                  approveBid={this.approve}
+                  isToggleOn={this.state.isToggleOn}/>
               }
-            if (notification.id == this.props.params.id) {
-              if(notification.notification_type == "pending_peyment"){
-                return<div key={index}>
-                  <h3 className="titleNotification"> {notification.bid.user.first_name} has ofer the
-                    job {notification.title}</h3>
-                  <p>This shipment must be picked up by  {moment(new Date(notification.bid.created_at)).format('MMMM Do YYYY')} and dropped off
-                    by {moment(new Date(notification.bid.updated_at)).format('MMMM Do YYYY')}</p>
-                  <p>The client has proposed this shipment for {notification.bid.budget} </p>
-                  <b>Other Notes:</b>
-                  <p>{notification.bid.description}</p>
-                  <p>Please ready the full terms here</p>
-                  <div className="button_accept">
-                    <Payment bid={{id: notification.bid.id}}/>
-                  </div>
-                </div>
-              }
-            } return null
-            }
+              return null
+            },
           )}
         </div>
       </div>
@@ -82,4 +52,4 @@ class Notification extends Component {
 const mapStateToProps = (state) => ({
   notifications: state.notifications_bid.data,
 })
-export default ReactRedux.connect(mapStateToProps, {approveBid})(Notification)
+export default connect(mapStateToProps, { approveBid })(Notification)
