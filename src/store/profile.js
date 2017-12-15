0b9b06723761295441ef40ac7097655a5f0bf50b
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2017-12-14 19:26:59 -0500

    change profile

--- src/store/profile.js
+++ src/store/profile.js
@@ -10,13 +10,13 @@ export default (state = initialState, action = {}) => {
   switch (action.type) {
     case GET_PROFILE:
       let profile = action.profile
-      profile.be_a_cerrier = false;
+      profile.be_a_carrier = false;
       profile.ship = false;
       if(typeof profile.purpose == 'string'){
         let types = profile.purpose.split('/').filter(item => {
-          return ['ship','be_a_cerrier'].indexOf(item) > -1
+          return ['ship','be_a_carrier'].indexOf(item) > -1
         })
-        profile.be_a_cerrier = types.indexOf('be_a_cerrier') > -1
+        profile.be_a_carrier = types.indexOf('be_a_carrier') > -1
         profile.ship = types.indexOf('ship') > -1
       }
 
