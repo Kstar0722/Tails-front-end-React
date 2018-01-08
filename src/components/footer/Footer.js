fc624358ac215d0ec31ecef56fc44c2ad99789e6
DESKTOP-JJRFLLJ\KStar <kstar722@hotmail.com>
2018-01-08 18:56:03 -0500

    change Header and Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -20,7 +20,6 @@ class Footer extends React.Component {
 						<nav className="hide-on-mobile">
 							<ul className="d-flex flex-row flex-wrap align-items-center justify-content-around">
 								<li className='menu-li-width'></li>
-								<li><Link to='/browse-jobs'>Browse Jobs</Link></li>
 								<li><Link to='/how-it-works'>How it Works</Link></li>
 								<li><Link to='/faq'>FAQ</Link></li>
 								<li><Link to='/support'>Support</Link></li>
@@ -32,7 +31,6 @@ class Footer extends React.Component {
 					</div>
 					<nav className="mobile-nav">
 						<ul className="d-flex flex-row flex-wrap align-items-center justify-content-around">
-							<li><Link to='/browse-jobs'>Browse Jobs</Link></li>
 							<li><Link to='/how-it-works'>How it Works</Link></li>
 							<li><Link to='/faq'>FAQ</Link></li>
 							<li><Link to='/support'>Support</Link></li>
